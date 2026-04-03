4bf740d528f6796008158239799ba051d07d5200
palex00 <palex000000@gmail.com>
2026-04-03 02:00:49 +0200

    forgot to change init

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,7 +3,7 @@ local variant = Tracker.ActiveVariantUID
 -- Items
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/events.json")
-Tracker:AddItems("items/settings.json")
+Tracker:AddItems("items/settings.jsonc")
 Tracker:AddItems("items/tools.json")
 
 -- Logic
@@ -11,20 +11,20 @@ ScriptHost:LoadScript("scripts/logic/utils.lua")
 ScriptHost:LoadScript("scripts/logic/logic.lua")
 
 -- Maps
-Tracker:AddMaps("maps/maps.json")
+Tracker:AddMaps("maps/maps.jsonc")
 Tracker:AddMaps("maps/main.json") -- this map image will change depending on setting. We initially load either the YAML-default.
 
 -- Locations
-Tracker:AddLocations("locations/access.json") -- all our location-based access logic goes in here
-Tracker:AddLocations("locations/overworld.json") -- the overworld map goes in here
-Tracker:AddLocations("locations/submaps.json") -- this entirely contains refs to the overworld map for submaps.
+Tracker:AddLocations("locations/access.jsonc") -- all our location-based access logic goes in here
+Tracker:AddLocations("locations/overworld.jsonc") -- the overworld map goes in here
+Tracker:AddLocations("locations/submaps.jsonc") -- this entirely contains refs to the overworld map for submaps.
 
 -- Layout
-Tracker:AddLayouts("layouts/settings_popup.json")
+Tracker:AddLayouts("layouts/settings_popup.jsonc")
 Tracker:AddLayouts("layouts/tools.json")
 Tracker:AddLayouts("layouts/items/items.json")
 Tracker:AddLayouts("layouts/tabs_single.json") -- we initially load the simple one. Watches & Toggle can change it by hotswapping it.
-Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/tracker.jsonc")
 Tracker:AddLayouts("layouts/broadcast.json") -- who even uses this.
 
 -- AutoTracking for Poptracker
